c14a309ab4b42caa4d30f3dd6c7938b358f262a6
Sanzhar <sanzhar.maratov@proton.me>
2024-01-12 10:40:40 +0600

    play target

---
+++ play/main.cpp
@@ -0,0 +1,23 @@
+#include "coroutines/coroutine.hpp"
+#include <cstdlib>
+#include <iostream>
+
+void func(int num) {
+	std::cout << "NUM: " << num << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+	Coroutine cr = Coroutine([&cr](){
+			func(2);
+			cr.Suspend();
+			func(4);
+			});
+
+	func(1);
+	cr.Resume();
+	func(3);
+	cr.Resume();
+	func(5);
+	
+	return EXIT_SUCCESS;
+}
